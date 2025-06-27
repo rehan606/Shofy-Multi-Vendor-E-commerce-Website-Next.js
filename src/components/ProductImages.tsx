b93565399e5c437f67bac220ffc9ea145facb39a
Rehan <rehanlemu@gmail.com>
2025-06-28 01:03:50 +0600

    Product detail page changle image by clicking another image

--- src/components/ProductImages.tsx
+++ src/components/ProductImages.tsx
@@ -1,18 +1,39 @@
+"use client"
+/* eslint-disable @typescript-eslint/no-unused-vars */
+
 import Image from 'next/image';
-import React from 'react'
+import React, { useState } from 'react'
 
 interface Props {
     images: string[];
 }
 
 const ProductImages = ({ images }: Props) => {
-  return (
-    <div>
-        <div>
-            <Image src={images?.[0]} alt='ProductImage' width={500} height={500}/>
+    const [currentImage, setCurrentImage] = useState(images[0])
+    return (
+        <div className='flex justify-between '>
+            <div>
+                {images?.map((item, index) => (
+                    <Image
+                        key={index} 
+                        src={item} 
+                        alt="productImage" 
+                        width={200} 
+                        height={200} 
+                        className={`w-24 h-24 object-contain cursor-pointer opacity-80 hover:opacity-100 duration-200 rounded-sm border border-gray-200 mb-1 p-1 ${currentImage === item && "border-gray-400 opacity-100"}`}
+                        onClick={() => setCurrentImage(item)}
+                    />
+                ))}
+            </div>
+
+            <div className='bg-gray-100 rounded-md ml-5 w-full max-h-[550px] '>
+                <Image 
+                    src={currentImage}  
+                    alt='ProductImage' width={500} height={500} 
+                    className='w-full h-full object-contain'/>
+            </div>
         </div>
-    </div>
-  )
+    )
 }
 
 export default ProductImages
