db9e9a9240563844cbf15e859268557afe768f27
Rehan <rehanlemu@gmail.com>
2025-07-05 02:07:17 +0600

    when quantity will be increase or decrease product price and discount price will be increase and decrease.

--- src/components/ProductPrice.tsx
+++ src/components/ProductPrice.tsx
@@ -1,8 +1,20 @@
-import React from 'react'
-import { ProductType } from '../../type';
+"use client";
+import React, { useEffect, useState } from 'react'
+import { ProductType, StateType } from '../../type';
 import PriceFormat from './PriceFormat';
+import { useSelector } from 'react-redux';
 
 const ProductPrice = ({ product }: {product: ProductType }) => {
+
+    const { cart } = useSelector((state:StateType) => state?.shopy);
+    const [existingProduct, setExistingProduct] = useState<ProductType | null> (null);
+
+    useEffect(() => {
+      const availableProduct = cart?.find((item) => item?.id === product?.id);
+      if(availableProduct){
+        setExistingProduct(availableProduct)
+      }
+    }, [cart, product]);
 
     const regularPrice = product?.price;
     const discountPrice = product?.price + product?.discountPercentage / 100 ;
@@ -10,8 +22,8 @@ const ProductPrice = ({ product }: {product: ProductType }) => {
 
     return (
       <div className="flex items-center gap-2">
-         <PriceFormat amount={discountPrice} className='text-gray-500 line-through font-normal'/>
-         <PriceFormat amount={regularPrice} className='text-sky-500 font-semibold'/>
+         <PriceFormat amount={existingProduct? discountPrice * existingProduct.quantity!: discountPrice} className='text-gray-500 line-through font-normal'/>
+         <PriceFormat amount={existingProduct? regularPrice * existingProduct.quantity!: regularPrice} className='text-sky-500 font-semibold'/>
          
       </div>
     )
