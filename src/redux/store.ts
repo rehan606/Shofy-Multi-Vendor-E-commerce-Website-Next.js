82f63e88c6ce1e2ff52f87769304855ceb443cbd
Rehan <rehanlemu@gmail.com>
2025-07-06 22:52:12 +0600

    add local and webstorage

--- src/redux/store.ts
+++ src/redux/store.ts
@@ -1,11 +1,33 @@
 import { configureStore } from '@reduxjs/toolkit'
 import shopyReducer from './shopySlice'
-import { persistStore, persistReducer } from 'redux-persist'
-import storage from 'redux-persist/lib/storage' // defaults to localStorage for web
+import { persistStore, persistReducer, WebStorage } from 'redux-persist'
+// import storage from 'redux-persist/lib/storage' // defaults to localStorage for web
+import createWebStorage from 'redux-persist/es/storage/createWebStorage'
 
+export function createPersistStore(): WebStorage {
+  const isServer = typeof window === 'undefined'
+
+  if(isServer) {
+    return {
+      getItem() {
+        return Promise.resolve(null);
+      },
+      setItem() {
+        return Promise.resolve();
+      },
+      removeItem() {
+        return Promise.resolve();
+      },
+    };
+  }
+  return createWebStorage("local");
+}
+
+const storage = typeof window !== 'undefined'? createWebStorage("local") : createPersistStore();
 
 const persistConfig = {
   key: 'root',
+  version: 1,
   storage,
 }
 
