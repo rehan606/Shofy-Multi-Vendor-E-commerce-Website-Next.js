8f9d84e045ecd6668b76aa46dc1acb9c60b948c1
Rehan <rehanlemu@gmail.com>
2025-07-07 22:12:01 +0600

    Shoping Cart Summary

--- src/components/cartPage/CartProducts.tsx
+++ src/components/cartPage/CartProducts.tsx
@@ -15,7 +15,7 @@ const CartProducts = () => {
                 <>
                     <h1 className='text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl'> Shopping Cart </h1>
 
-                    <div className='mt-10 lg:grid lg:grid-cols-12 lg:items-start lg:gap-x-1 2'>
+                    <div className='mt-10 lg:grid lg:grid-cols-12 lg:items-start lg:gap-x-12'>
                         <section className='lg:col-span-7'>
                             <div className='divide-y divide-gray-200 border-b border-t border-gray-200'>
                                 {cart?.map((product:ProductType) => (
@@ -24,7 +24,7 @@ const CartProducts = () => {
                             </div>
                         </section>
                         {/* Cart Summary  */}
-                        <CartSummary/>
+                        <CartSummary cart={cart} />
                     </div>
                 </> 
                 : 
--- src/components/cartPage/CartSummary.tsx
+++ src/components/cartPage/CartSummary.tsx
@@ -1,9 +1,54 @@
-import React from 'react'
+/* eslint-disable @typescript-eslint/no-non-null-asserted-optional-chain */
+import React, { useEffect, useState } from 'react'
+import Title from '../Title'
+import PriceFormat from '../PriceFormat'
+import { ProductType } from '../../../type'
+import Button from '../header/Button';
+import toast from 'react-hot-toast';
 
-const CartSummary = () => {
+interface Props {
+    cart: ProductType[];
+}
+
+
+const CartSummary = ({ cart }: Props) => {
+    const [totalAmt, setTotalAmt] = useState(0);
+    const [discountAmt, setDiscountAmt] = useState(0);
+
+    useEffect(() => {
+        let amt = 0;
+        let discount = 0;
+
+        cart?.map((item) => {
+            amt += item?.price * item?.quantity!;
+            discount += ((item?.price * item?.quantity!) / 100) * item?.quantity!;
+        });
+        setTotalAmt(amt)
+        setDiscountAmt(discount)
+    },[cart]);
+
+    const handleCheckout = () => {
+        toast.success('Chackout is comming soon!');
+    }
+
     return (
-        <div>
-            
+        <div className='bg-gray-100 rounded-lg px-4 py-6 sm:p-10 lg:col-span-5 mt-10 lg:mt-0'>
+            <Title>Cart Summary</Title>
+            <div className='flex flex-col gap-3 mt-5'>
+                <div className='flex items-center justify-between'>
+                    <Title className='text-lg font-medium '>Sub Total</Title>
+                    <PriceFormat amount={totalAmt}/> 
+                </div>
+                <div className='flex items-center justify-between'>
+                    <Title className='text-lg font-medium '>Discount</Title>
+                    <PriceFormat amount={discountAmt}/> 
+                </div>
+                <div className='flex items-center justify-between'>
+                    <Title className='text-lg font-medium '>Payable Amount</Title>
+                    <PriceFormat amount={totalAmt - discountAmt} className='text-lg font-bold' /> 
+                </div>
+                <Button onClick={handleCheckout}>Checkout</Button>
+            </div>
         </div>
     )
 }
