1329902dfc10b3f996ce2f35928fb301c770ccb5
Rehan <rehanlemu@gmail.com>
2025-07-07 22:59:26 +0600

    Display search items

--- src/components/header/SearchInput.tsx
+++ src/components/header/SearchInput.tsx
@@ -4,6 +4,8 @@ import { getData } from '@/helpers';
 import React, { useEffect, useState } from 'react'
 import { RiCloseLine, RiSearchLine } from 'react-icons/ri';
 import { ProductType } from '../../../type';
+import Link from 'next/link';
+import { CiSearch } from 'react-icons/ci';
 
 const SearchInput = () => {
 
@@ -42,6 +44,39 @@ const SearchInput = () => {
             {search && ( <RiCloseLine onClick={() => setSearch('')} className='text-xl absolute top-2.5 right-12 text-gray-500 hover:text-red-500 cursor-pointer duration-200'/>)}
 
             <span className=' w-10 h-10 bg-[#2269bb] inline-flex items-center justify-center text-white cursor-pointer absolute top-0 right-0 hover:bg-[#5a7ca3]'> <RiSearchLine/> </span>
+
+            {/* Search Product  */}
+            {search && (
+                <div className='absolute left-0 top-12 w-full mx-auto h-auto max-h-96 bg-white rounded-md overflow-y-scroll cursor-pointer text-black p-4'>
+                    {filteredProducts?.length > 0 ? 
+                        (<div>
+                            {filteredProducts?.map((item:ProductType) => (
+                            
+                                <Link 
+                                    key={item?.id}
+                                    href={{
+                                        pathname: `/products/${item?.id}`,
+                                        query: { id: item?.id },
+                                    }} 
+                                    onClick={() => setSearch("")}
+
+                                    className='flex items-center gap-x-2 text-base font-medium hover:bg-gray-400 px-3 py-1.5'
+                                    >
+                                    <CiSearch className='text-lg'/> {item?.title}
+                                </Link>
+                            ))}
+                        </div> 
+                        ) : (
+                        <div className='py-10 px-5'>
+                            <p>
+                                Nothing Matched With {" "} <span className='font-semibold underline underline-offset-2 decoration-[1px]'>{search}</span> {" "} Please Try Again.
+                            </p>
+                        </div>)
+                    }
+                </div>
+            )}
+
+
         </div>
     )
 }
