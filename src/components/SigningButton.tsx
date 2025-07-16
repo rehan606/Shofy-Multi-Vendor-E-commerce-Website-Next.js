141306208324754869815f3a27d4aa2e8a422855
Rehan <rehanlemu@gmail.com>
2025-07-16 12:52:26 +0600

    NextAuth Configuration

--- src/components/SigningButton.tsx
+++ src/components/SigningButton.tsx
@@ -1,18 +1,17 @@
-
+"use client"
+import { signIn } from 'next-auth/react'
 import React from 'react'
 import { LiaUser } from 'react-icons/lia'
 
 const SigningButton = () => {
     return (
-        <div>
-            <div className='flex items-center gap-2 cursor-pointer text-sm'>
-                <div className='border-2 border-gray-700 p-1.5 rounded-full text-xl'>
-                    <LiaUser/>
-                </div>
-                <div>
-                    <p className='text-xs'>Hello, Guests</p>
-                    <p className='font-medium'>Login / Register</p>
-                </div>
+        <div onClick={() => signIn() } className='flex items-center gap-2 cursor-pointer text-sm'>
+            <div className='border-2 border-gray-700 p-1.5 rounded-full text-xl'>
+                <LiaUser/>
+            </div>
+            <div>
+                <p className='text-xs'>Hello, Guests</p>
+                <p className='font-medium'>Login / Register</p>
             </div>
         </div>
     )
